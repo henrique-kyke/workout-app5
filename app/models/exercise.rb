9b67320ad8c7f7e1903174316e53c9f1245cbf30
Henrique Santana <henrique.santana@boo-box.com>
2017-07-14 19:01:41 -0300

    Validate exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,5 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+  validates_presence_of :duration_in_min, :workout, :workout_date
+  validates :duration_in_min, numericality: true
 end
--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -4,23 +4,36 @@ RSpec.feature "Creating Exercises" do
   before do
     @john = User.create(email: "john@example.com", password: "password")
     login_as(@john)
-  end
 
-  scenario "with valid inputs" do
     visit "/"
     click_link "My Lounge"
     click_link "New Workout"
+
+  end
+
+  scenario "with valid inputs" do
+
     expect(page).to have_link("Back")
 
     fill_in "Duration", with: 70
     fill_in "Workout details", with: "Weight lifting"
     fill_in "Activity date", with: "2017-07-03"
-    click_button "Create Exercise"
+    click_button I18n.t("helpers.submit.create", model: Exercise)
 
     expect(page).to have_content("Exercise has been created")
 
     exercise = Exercise.last
     expect(current_path).to eq(user_exercise_path(@john, exercise))
     expect(exercise.user_id).to eq(@john.id)
+  end
+
+  scenario "with invalid inputs" do
+
+    fill_in "Duration", with: "A"
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).to have_content I18n.t("errors.messages.not_a_number")
+    expect(page).to have_content I18n.t("errors.messages.blank")
   end
 end
