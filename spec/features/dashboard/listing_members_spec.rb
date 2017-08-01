a45d050fd473a90fbea7aef839ec86825b41e5db
Henrique Santana <henrique.santana@boo-box.com>
2017-08-01 18:49:17 -0300

    List members

---
+++ spec/features/dashboard/listing_members_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.feature "Listing Members" do
+  before do
+    @john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+    login_as(@john)
+
+    @sarah = User.create(first_name: "Sarah", last_name: "Jones", email: "sarah@example.com", password: "password")
+    login_as(@john)
+  end
+
+  scenario "shows a list of registred members" do
+    visit "/"
+
+    expect(page).to have_content("List of Members")
+    expect(page).to have_content(@john.full_name)
+    expect(page).to have_content(@sarah.full_name)
+  end
+end
