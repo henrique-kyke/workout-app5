c995107eb487e4f79640a3455b692942220c0f4f
Henrique Santana <henrique.santana@boo-box.com>
2017-08-18 16:47:03 -0300

    Add pagination

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   # before_action :authenticate_user!
   def index
-    @athletes = User.all
+    @athletes = User.paginate(:page => params[:page])
   end
 end
