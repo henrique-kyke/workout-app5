44be6204621994de9d52ff4c8ceecb888f7b931b
Henrique Santana <henrique.santana@boo-box.com>
2017-10-09 14:28:21 -0300

    Create chatroom when user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :last_name
 
+  after_create :create_chatroom
+
   self.per_page = 10
 
   has_many :exercises
   has_many :friendships
   has_many :friends, through: :friendships, class_name: "User"
+  has_one :room
 
   def full_name
     [first_name, last_name].join(" ")
@@ -34,5 +37,11 @@ class User < ApplicationRecord
 
   def current_friendship(friend)
     friendships.where(friend: friend).first
+  end
+
+  private
+  def create_chatroom
+    hyphenated_username = self.full_name.split.join("-")
+    Room.create(name: hyphenated_username, user_id: self.id)
   end
 end
